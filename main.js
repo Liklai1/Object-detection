76052c7e5d8d13bcb5197af4b5f5c48a5ee5e9bc
dracomalfoy13 <75836042+dracomalfoy13@users.noreply.github.com>
2021-09-18 15:25:23 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,10 +1,13 @@
 var img;
+var status="";
 function preload(){
     img=loadImage('dog_cat.jpg');
 }
 function setup(){
     canvas= createCanvas(640, 420);
     canvas.center();
+    objectDetector=ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML="status : detecting objects";
 }
 function draw(){
     image(img, 0 , 0 , 640 , 420);
@@ -13,4 +16,21 @@ function draw(){
     noFill();
     stroke("red");
     rect(30, 60, 450, 350);
+    fill("red");
+    text("cat", 320, 120);
+    noFill();
+    stroke("red");
+    rect(300, 90, 270, 320);
+}
+function modelLoaded(){
+    status=true;
+    objectDetector.detect(img, gotResult);
+}
+function gotResult(error, results){
+    if(error){
+        console.error(error);
+    }
+    else{
+        console.log(results);
+    }
 }
